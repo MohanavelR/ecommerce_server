3deaa203570bbefd76ce602bd3a7ac41d1d65eea
MohanavelR <rmohanavel2003@gmail.com>
2025-10-10 19:09:51 +0530

    vercel

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://127.0.0.1:5173"
+  "http://127.0.0.1:5173",
+  "https://ecommerce-ui-mocha.vercel.app"
 ];
 app.use(cors({
     origin:allowedOrigins,
